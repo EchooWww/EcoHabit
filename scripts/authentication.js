dbb4c1353b79a7cd5efe57d0a9ab39449f6dd8b8
ECHOOWWW <ywang623@my.bcit.com>
2023-03-05 16:37:55 -0800

    Edited authentification.js to create a sub-collection of habits and default documents for every new user.

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -4,17 +4,6 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      //------------------------------------------------------------------------------------------
-      // The code below is modified from default snippet provided by the FB documentation.
-      //
-      // If the user is a "brand new" user, then create a new "user" in your own database.
-      // Assign this user with the name and email provided.
-      // Before this works, you must enable "Firestore" from the firebase console.
-      // The Firestore rules must allow the user to write. 
-      //------------------------------------------------------------------------------------------
       var user = authResult.user;                            // get the user object from the Firebase authentication database
       if (authResult.additionalUserInfo.isNewUser) {         //if new user
         db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
@@ -22,6 +11,39 @@ var uiConfig = {
           email: user.email,                         //with authenticated user's ID (user.uid)
         }).then(function () {
           console.log("New user added to firestore");
+          // add a subcollection of "habits" to the new user
+          db.collection("users").doc(user.uid).collection("habits");
+          // add default habits to the subcollection
+          db.collection("users").doc(user.uid).collection("habits").add({
+            name: "Ride a bike🚴",
+            count: 0,
+            continuous_count: 0,
+            last_checked: null
+          });
+          db.collection("users").doc(user.uid).collection("habits").add({
+            name: "Eat less meat🥩",
+            count: 0,
+            continuous_count: 0,
+            last_checked: null
+          });
+          db.collection("users").doc(user.uid).collection("habits").add({
+            name: "Do recycling🚮",
+            count: 0,
+            continuous_count: 0,
+            last_checked: null
+          });
+          db.collection("users").doc(user.uid).collection("habits").add({
+            name: "Use reusable bags🛍️",
+            count: 0,
+            continuous_count: 0,
+            last_checked: null
+          });
+          db.collection("users").doc(user.uid).collection("habits").add({
+            name: "Save water💧",
+            count: 0,
+            continuous_count: 0,
+            last_checked: null
+          });
           window.location.assign("main.html");       //re-direct to main.html after signup
         }).catch(function (error) {
           console.log("Error adding new user: " + error);
