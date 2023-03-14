af181e3a16c1fe14dc165209d78d5c1aa1c00695
ECHOOWWW <ywang623@my.bcit.com>
2023-03-14 15:28:33 -0700

    conbined main page and list page, changed navbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,67 @@
 const habitList = document.querySelector('.todo-list');
+const addHabitButton = document.querySelector('.todo__add');
+addHabitButton.style.display = 'flex';
+addHabitButton.style.alignItems = 'center';
+addHabitButton.style.marginLeft = '15%';
+addHabitButton.style.marginRight = '15%';
+addHabitButton.style.marginTop = '0';
+addHabitButton.style.paddingTop = '0';
+
+// Array to store changes made to habit items
+let habitChanges = [];
+
+addHabitButton.addEventListener('click', () => {
+  const habitName = prompt('Enter a new habit:');
+  if (habitName) {
+    addHabitItem(habitName);
+    // Add new habit to changes array
+    habitChanges.push({ type: 'add', name: habitName });
+  }
+});
+
+
+
 function addHabitItem(name, id) {
   const habitItem = document.createElement('label');
   habitItem.classList.add('todo');
+  habitItem.style.display = 'flex';
+  habitItem.style.alignItems = 'center';
+  habitItem.style.marginLeft = '15%';
+  habitItem.style.marginRight = '15%';
+
+  const checkboxContainer = document.createElement('div');
+  checkboxContainer.classList.add('todo__container');
 
   const checkbox = document.createElement('input');
   checkbox.classList.add('todo__state');
   checkbox.type = 'checkbox';
   habitItem.appendChild(checkbox);
 
+  const deleteButton = document.createElement('button');
+  deleteButton.classList.add('btn', 'btn-link', 'text-decoration-none');
+  deleteButton.textContent = '×';
+  deleteButton.style.fontSize = '35pt'
+  deleteButton.style.color = '#6096B4';
+  deleteButton.style.fontWeight = 'bold';
+  deleteButton.style.position = 'absolute';
+  deleteButton.style.right = '0';
+
+
+  deleteButton.addEventListener('click', () => {
+    const confirmDelete = confirm(`Are you sure to remove ${name} from your habit list?`);
+    if (confirmDelete) {
+      const habitId = habitItem.getAttribute('data-id');
+      habitList.removeChild(habitItem);
+      // Add habit removal to changes array
+      habitChanges.push({ type: 'remove', name: name });
+      saveHabitsToFirestore();
+    }
+  });
   const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   svg.setAttribute('viewBox', '0 0 200 25');
   svg.classList.add('todo__icon');
   habitItem.appendChild(svg);
+  habitItem.insertBefore(deleteButton, svg.nextSibling); // insert delete button after checkbox
 
   const lineUse = document.createElementNS('http://www.w3.org/2000/svg', 'use');
   lineUse.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#todo__line');
@@ -28,19 +78,17 @@ function addHabitItem(name, id) {
   checkUse.classList.add('todo__check');
   svg.appendChild(checkUse);
 
-  const circleUse = document.createElementNS('http://www.w3.org/2000/svg', 'use');
-  circleUse.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#todo__circle');
-  circleUse.classList.add('todo__circle');
-  svg.appendChild(circleUse);
 
   const textDiv = document.createElement('div');
   textDiv.classList.add('todo__text');
   textDiv.textContent = name;
   habitItem.appendChild(textDiv);
-
   habitItem.setAttribute('data-id', id);
+  habitItem.appendChild(deleteButton);
   habitList.appendChild(habitItem);
 }
+
+
 
 
 // Load the user's habit list from firestore
@@ -58,3 +106,33 @@ function loadHabitsFromFirestore() {
   });
 }
 loadHabitsFromFirestore();
+
+function saveHabitsToFirestore() {
+  const userID = firebase.auth().currentUser.uid;
+  const batch = db.batch();
+
+  habitChanges.forEach((change) => {
+    const habitRef = db.collection('users').doc(userID).collection('habits').doc(); // get a new document ID
+    if (change.type === 'add') {
+      batch.set(habitRef, {
+        name: change.name,
+        count: 0,
+        continious_count: 0,
+        last_checked: null
+      });
+    } else if (change.type === 'remove') {
+      const habitDocRef = db.collection('users').doc(userID).collection('habits');
+      habitDocRef.where("name", "==", change.name).get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          doc.ref.delete();
+        });
+      });
+    }
+  });
+
+  batch.commit().then(() => {
+    console.log('Batch write to Firestore successful');
+  }).catch((error) => {
+    console.error('Error writing batch to Firestore: ', error);
+  });
+}
