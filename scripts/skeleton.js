c97b067cf1bdd68e7cb71dba54abe14b16591a2f
ECHOOWWW <ywang623@my.bcit.com>
2023-03-15 18:38:26 -0700

    added node and server.js, resorted the documents, and made the chatbot functional

--- scripts/skeleton.js
+++
@@ -1,6 +0,0 @@
-function loadSkeleton() {
-  console.log($('#headerPlaceholder').load('./text/header.html'));
-  console.log($('#navbarPlaceholder').load('./text/nav.html'));
-}
-loadSkeleton();  
-
