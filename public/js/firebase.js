6429e0cb95778b69e33c44d147edb2e679cb38a1
Azura Rocks <lasai.azura.maya@gmail.com>
2023-03-28 09:44:05 -0700

    user profile page modified (2hrs)

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,7 +1,6 @@
-// import { initializeApp } from "firebase/app";
+// //  import { initializeApp } from "firebase/app";
 // import { getStorage } from "firebase/storage";
 
-// i added these keys to before and after the API keys in firebaseAPI_BBY14.js
 
 
 
--- public/js/profile.js
+++ public/js/profile.js
@@ -1,42 +1,93 @@
-import "./profile.css"
-import { Avatar } from "@mui/material";
-import { useState } from "react";
-import { storage } from "./firebase";
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
-function App() {
-    const [image, setImage] = useState(null);
-    const [url, setUrl] = useState(null);
+var ImageFile;      //global variable to store the File Object reference
 
-    const handleImageChange = (e) => {
-        if (e.target.files[0]) {
-            setImage(e.target.files[0])
-        }
-    };
-    const handleSubmit = () => {
-        const imageRef = ref(storage, "image");
-        uploadBytes(imageRef, image).then(() => {
-            getDownloadURL(imageRef).then((url) => {
-                setUrl(url)
+function chooseFileListener(){
+    const fileInput = document.getElementById("mypic-input");   // pointer #1
+    const image = document.getElementById("mypic-goes-here");   // pointer #2
+
+    //attach listener to input file
+    //when this file changes, do something
+    fileInput.addEventListener('change', function(e){
+
+        //the change event returns a file "e.target.files[0]"
+	      ImageFile = e.target.files[0];
+        var blob = URL.createObjectURL(ImageFile);
+
+        //change the DOM img element source to point to this file
+        image.src = blob;    //assign the "src" property of the "img" tag
+    })
+}
+chooseFileListener();
+
+function saveUserInfo() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        var storageRef = storage.ref("images/" + user.uid + ".jpg");
+
+        //Asynch call to put File Object (global variable ImageFile) onto Cloud
+        storageRef.put(ImageFile)
+            .then(function () {
+                console.log('Uploaded to Cloud Storage.');
+
+                //Asynch call to get URL from Cloud
+                storageRef.getDownloadURL()
+                    .then(function (url) { // Get "url" of the uploaded file
+                        console.log("Got the download URL.");
+												//get values from the from
+                        userName = document.getElementById('nameInput').value;
+                        userCity = document.getElementById('cityInput').value;
+
+                        //Asynch call to save the form fields into Firestore.
+                        db.collection("users").doc(user.uid).update({
+                                name: userName,
+                                city: userCity,
+                                profilePic: url // Save the URL into users collection
+                            })
+                            .then(function () {
+                                console.log('Added Profile Pic URL to Firestore.');
+                                console.log('Saved use profile info');
+                                document.getElementById('personalInfoFields').disabled = true;
+                            })
+                    })
             })
-                .catch(error => {
-                    console.log(error.message, "error image url");
-                });
-            setImage(null);
-                .catch(error => {
-                console.log(error.message, "error image url");
-            });
-    };
-
-    return (
-        <div className="App">
-            <Avatar src={url} sx={{ width: 100, height: 100 }} />
-            <input type="file" onChange={handleImageChange} />
-            <button onClick={handleSubmit}>Submit</button>
-        </div>
-    );
+    })
 }
 
-export default App;
 
+function populateInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                // go and get the curret user info from firestore
+                currentUser = db.collection("users").doc(user.uid);
 
+                currentUser.get()
+                    .then(userDoc => {
+                        let userName = userDoc.data().name;
+                        let userCity = userDoc.data().city;
+                        let picUrl = userDoc.data().profilePic; 
+
+                        if (userName != null) {
+                            document.getElementById("nameInput").value = userName;
+                        }
+                       if (userCity != null) {
+                            console.log(userCity)
+                            document.getElementById("cityInput").value = userCity;
+                        }
+                        if (picUrl != null){
+                            console.log(picUrl);
+								            // use this line if "mypicdiv" is a "div"
+                            //$("#mypicdiv").append("<img src='" + picUrl + "'>")
+                            $("#mypic-goes-here").attr("src", picUrl);
+                        }
+                        else
+                        console.log("picURL is null");
+                    })
+
+            } else {
+                console.log("no user is logged in")
+            }
+        }
+
+    )
+
+}
+populateInfo();
